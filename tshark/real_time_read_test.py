e27b97f6b6258e7fd76bbeb459c3dce0389a55d7
rhish9h <rhish9h@gmail.com>
2020-02-10 21:53:51 +0530

    code for reading updating file

---
+++ tshark/real_time_read_test.py
@@ -0,0 +1,19 @@
+# follow.py
+#
+# Follow a file like tail -f.
+
+import time
+def follow(thefile):
+    thefile.seek(0,2)
+    while True:
+        line = thefile.readline()
+        if not line:
+            time.sleep(0.1)
+            continue
+        yield line
+
+if __name__ == '__main__':
+    logfile = open("file.csv","r")
+    loglines = follow(logfile)
+    for line in loglines:
+        print (line)
